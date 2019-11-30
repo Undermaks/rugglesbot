597370805d2fb143e0609a9b3f6e65857ff0d7ea
UnderMaks.DEV <48382389+UnderMaksDev@users.noreply.github.com>
2019-11-30 20:32:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,12 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+var prefix ="*"
 
+bot.on('ready', () => {
+  console.log('Запустился!');
+});
+
+bot.on('ready', () => {
 
 //Ниже не трогать!
 bot.login(process.env.BOT_TOKEN);
