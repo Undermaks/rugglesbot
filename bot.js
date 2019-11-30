0b78e90c7cd73936891158c6fcb1d084a0518a01
UnderMaks.DEV <48382389+UnderMaksDev@users.noreply.github.com>
2019-11-30 23:33:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,8 @@ bot.on('ready', () => {
 
   bot.on('ready', ()  => {
 let channel = bot.channels.get("650447242381099018")
-if(bot.guild.memberCount >  0){
-channel.edit({ name: "На сервере: "+ bot.guild.memberCount +"челов"})
+if(bot.g >  0){
+channel.edit({ name: "На сервере: "+ bot.guild.members +"челов"})
   }
 });
   
