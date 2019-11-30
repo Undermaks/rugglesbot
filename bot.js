8a9a36a3beafa4093042778064e3da661c2402b0
UnderMaks.DEV <48382389+UnderMaksDev@users.noreply.github.com>
2019-11-30 22:27:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-var prefix ="*"
+var prefix ="!"
 
 bot.on('ready', () => {
   console.log('Запустился!');
 });
 
 bot.on('ready', () => {
-  // Выше не трогать
-  
-  bot.on('ready', () => {
     bot.user.setStatus('available') // Can be 'available', 'idle', 'dnd', or 'invisible'
     bot.user.setPresence({
         game: {
-            name: ("vk.com/ruggles1337 | *help"),
+            name: ('vk.com/undermaks | *help'),
             type: 3
         }
     });
 });
-  
-  
-  
   
   
   
