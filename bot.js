170f4366cc299a1b6e472ba9b7e28c43f341f10d
UnderMaks.DEV <48382389+UnderMaksDev@users.noreply.github.com>
2019-11-30 20:36:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,24 @@ bot.on('ready', () => {
 bot.on('ready', () => {
   // Выше не трогать
   
+  bot.on('ready', () => {
+    bot.user.setStatus('available') // Can be 'available', 'idle', 'dnd', or 'invisible'
+    bot.user.setPresence({
+        game: {
+            name: ("vk.com/ruggles1337 | *help"),
+            type: 3
+        }
+    });
+});
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 });
 //Ниже не трогать!
